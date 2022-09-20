4a9da91dbca8b2c265d44362ddfcc315d84958fe
Sagadat Kuandykov <sgkuandykov@gmail.com>
2022-09-20 21:09:24 +0600

    design for IntroductionFragment.kt, draw button,img view, text view for this activity

---
+++ app/src/main/java/saga/io/ecommerce_app_kelineyt/fragments/loginRegister/AccountOptions.kt
@@ -0,0 +1,7 @@
+package saga.io.ecommerce_app_kelineyt.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import saga.io.ecommerce_app_kelineyt.R
+
+class AccountOptions : Fragment(R.layout.fragment_account_option) {
+}
---
+++ app/src/main/java/saga/io/ecommerce_app_kelineyt/fragments/loginRegister/LoginFragment.kt
@@ -0,0 +1,7 @@
+package saga.io.ecommerce_app_kelineyt.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import saga.io.ecommerce_app_kelineyt.R
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+}
