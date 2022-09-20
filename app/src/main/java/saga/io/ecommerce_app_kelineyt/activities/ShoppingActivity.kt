d17fac6d5bbdafbf149d929cebba0a6fcea3f9d5
Sagadat Kuandykov <sgkuandykov@gmail.com>
2022-09-20 18:16:10 +0600

    Initial commit

---
+++ app/src/main/java/saga/io/ecommerce_app_kelineyt/activities/ShoppingActivity.kt
@@ -0,0 +1,12 @@
+package saga.io.ecommerce_app_kelineyt.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import saga.io.ecommerce_app_kelineyt.R
+
+class ShoppingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shoping)
+    }
+}
